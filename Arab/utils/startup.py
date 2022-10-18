fd8ee3292434aaffd1d8d1bae288eff8c870ad71
alrys1 <116121942+alrys1@users.noreply.github.com>
2022-10-19 00:56:20 +0300

    Update startup.py

--- Arab/utils/startup.py
+++ Arab/utils/startup.py
@@ -15,7 +15,7 @@ from ..sql_helper.global_collection import del_keyword_collectionlist, get_item_
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 from .klanr import load_module
 from .tools import create_supergroup
-LOGS = logging.getLogger("تليثون العرب \n ")
+LOGS = logging.getLogger("سيدا ثون \n ")
 cmdhr = Config.COMMAND_HAND_LER
 async def load_plugins(folder):
     path = f"Arab/{folder}/*.py"
@@ -46,7 +46,7 @@ async def load_plugins(folder):
 async def startupmessage():
     try:
         if BOTLOG:
-            Config.CATUBLOGO = await iqthon.tgbot.send_file(BOTLOG_CHATID, "https://telegra.ph/file/74066cb3ddb0bdba1c4b7.mp4", caption="🝳 ⦙ تـمّ  اعـادة تشـغيل\n تليثـون العـرب ✓  :  [ 7.7 ] .\n\n🝳 ⦙ للحصول على اوامر السورس\n أرسـل : (  `.اوامري`  ) \n\n🝳 ⦙ لمـعرفة كيفية تغير بعض كلايش\n او صور السـورس  أرسـل  :\n (  `.مساعده`  )\n\n🝳 ⦙ القناة الرسمية تليثون العرب : @IQTHON\n🝳 ⦙ فارات سورس تليثون  :@TEAMTELETHON \n🝳 ⦙ كلايش تليثون :  @FGFFG\n 🝳 ⦙التحديثات والاضافات :  @M4_STORY\n",                buttons=[(Button.url("مطور تليثون الرسمي", "https://t.me/lll5l"),)],            )
+            Config.CATUBLOGO = await iqthon.tgbot.send_file(BOTLOG_CHATID, "https://telegra.ph/file/c0e6d1301e01d34cdc558.jpg", caption="🝳 ⦙ تـمّ  اعـادة تشـغيل\n سيدا ثون ✓  :  [ 7.7 ] .\n\n🝳 ⦙ للحصول على اوامر السورس\n أرسـل : (  `.اوامري`  ) ",                buttons=[(Button.url("مطور سيدا ثون الرسمي", "https://t.me/UUUO1944"),)],            )
     except Exception as e:
         LOGS.error(e)
         return None
@@ -100,7 +100,7 @@ async def verifyLoggerGroup():
     else:
         descript = "🝳 ︙ لا تحذف هذه المجموعة أو تغير إلى مجموعة (إذا قمت بتغيير المجموعة ، فسيتم فقد كل شيئ .)"
         iqphoto1 = await iqthon.upload_file(file="SQL/extras/iqthon1.jpg")
-        _, groupid = await create_supergroup(            "تخزين تليثون العرب العام", iqthon, Config.TG_BOT_USERNAME, descript  ,  iqphoto1 )
+        _, groupid = await create_supergroup(            "تخزين سيدا ثون العام", iqthon, Config.TG_BOT_USERNAME, descript  ,  iqphoto1 )
         addgvar("PRIVATE_GROUP_BOT_API_ID", groupid)
         print("🝳 ︙ تم إنشاء مجموعة المسـاعدة بنجاح وإضافتها إلى المتغيرات.")
         flag = True
@@ -119,9 +119,9 @@ async def verifyLoggerGroup():
         except Exception as e:
             LOGS.error(                "🝳 ︙ حدث استثناء عند محاولة التحقق من PM_LOGGER_GROUP_ID.\n" + str(e)            )
     else:
-        descript = "🝳 ︙ وظيفه هذا المجموعة لحفض رسائل التي تكون موجة اليك ان لم تعجبك هذا المجموعة قم بحذفها نهائيأ 👍 \n  الـسورس : - @IQTHON"
+        descript = "🝳 ︙ وظيفه هذا المجموعة لحفض رسائل التي تكون موجة اليك ان لم تعجبك هذا المجموعة قم بحذفها نهائيأ 👍 \n  الـسورس : - @UUUO1944"
         iqphoto2 = await iqthon.upload_file(file="SQL/extras/iqthon2.jpg")
-        _, groupid = await create_supergroup(            "تخزين تليثون العرب الخاص", iqthon, Config.TG_BOT_USERNAME, descript    , iqphoto2  )
+        _, groupid = await create_supergroup(            "تخزين سيدا ثون الخاص", iqthon, Config.TG_BOT_USERNAME, descript    , iqphoto2  )
         addgvar("PM_LOGGER_GROUP_ID", groupid)
         print("🝳 ︙ تم إنشاء مجموعة خاصة لـ PRIVATE_GROUP_BOT_API_ID بنجاح وإضافتها إلى المتغيرات.")
         flag = True
