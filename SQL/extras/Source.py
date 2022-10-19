71013bed99a34b49dd4841ae928bc2ce1f1257f1
alrys1 <116121942+alrys1@users.noreply.github.com>
2022-10-19 13:53:06 +0300

    Update Source.py

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -5,7 +5,7 @@ from Arab import StartTime, iqthon
 from Arab.Config import Config
 from Arab.plugins import mention
 help1 = ("**🝳 ⦙ كيفيه التنصيب :**")
-help2 = ("**🝳 ⦙ قـائمـه الاوامـر :**\n**🝳 ⦙ قنـاه السـورس :** @IQTHON\n**🝳 ⦙ شـرح اوامـر السـورس : @L3LL3**\n**🝳 ⦙ شـرح فـارات السـورس : @TEAMTELETHON** \n - اوامر الاونلاين تشتغل فقط في المجموعات ")
+help2 = ("**🝳 ⦙ قـائمـه الاوامـر :**\n**🝳 ⦙ قنـاه السـورس :** @UUUO1944  ")
 TG_BOT = Config.TG_BOT_USERNAME
 TM = time.strftime("%I:%M")
-Sour = f"**‎⿻┊My 𖠄 {mention} ٫ **\n‌‎**⿻┊BoT 𖠄 {TG_BOT} ٫**\n**‌‎⿻┊TimE 𖠄 {TM} ٫**\n**‌‎⿻┊‌‎VeRsIoN 𖠄 (7.7) ,** \n**⿻┊‌‎TeLeThoN AraB 𖠄** @IQTHON"
+Sour = f"**‎⿻┊My 𖠄 {mention} ٫ **\n‌‎**⿻┊BoT 𖠄 {TG_BOT} ٫**\n**‌‎⿻┊TimE 𖠄 {TM} ٫**\n**‌‎⿻┊‌‎VeRsIoN 𖠄 (7.7) ,** \n**⿻┊‌‎Mrs. Thon 𖠄** @UUUO1944"
