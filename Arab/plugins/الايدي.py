e4ea95f69f79eb0fdc89ebf0ce23761351aeaf0f
alrys1 <116121942+alrys1@users.noreply.github.com>
2022-10-20 14:45:22 +0300

    Update الايدي.py

--- Arab/plugins/الايدي.py
+++ Arab/plugins/الايدي.py
@@ -15,7 +15,7 @@ from ..sql_helper.globals import gvarstatus
 from . import spamwatch
 plugin_category = "@iqthon"
 LOGS = logging.getLogger(__name__)
-iqthon_TEXT = gvarstatus("CUSTOM_ALIVE_TEXT") or "╮•• مـعلومات الـشخص من بوت تليثون العرب"
+iqthon_TEXT = gvarstatus("CUSTOM_ALIVE_TEXT") or "╮•• مـعلومات الـشخص من بوت سيدا ثون"
 iqthonM = gvarstatus("CUSTOM_ALIVE_EMOJI") or "✦"
 iqthonF = gvarstatus("CUSTOM_ALIVE_FONT") or "★•┉ ┉ ┉ ┉ ┉ ┉  ┉ ┉ ┉ ┉•★"
 
